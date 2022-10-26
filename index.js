dfdd08d329e72109655c30200726ec15d7e1b871
Palash Ahmed <palashbiochem@gmail.com>
2022-10-26 15:43:01 +0600

    details json update

--- index.js
+++ index.js
@@ -6,13 +6,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./Data/courses.json');
+const details = require('./Data/details.json');
 
 app.get('/', (req, res)=> {
     res.send('Course Details API running');
 });
 
-app.get('/all-courses', (req, res)=>{
+app.get('/course-category', (req, res)=>{
     res.send(courses)
+});
+
+app.get('/course/:id', (req, res)=>{
+    const id = req.params.id;
+
+    if(id === '08'){
+        res.send(details);
+    }
+    else{
+        const course_details = details.filter(detail=> detail.category_id === id);
+        res.send(course_details);
+    }
+})
+
+app.get('/details', (req, res)=>{
+    res.send(details);
+})
+
+app.get('/details/:id', (req, res)=> {
+    const id = req.params.id;
+    const courseDetails= details.find(detail => detail._id === id);
+    res.send(courseDetails);
 })
 
 app.listen(port, ()=>{
